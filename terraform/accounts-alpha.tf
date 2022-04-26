73ef6cddc3d53585cddc01e1f9b8ead5b7e226a0
Charlie Stanley <willard.stanley@robinhood.com>
2022-04-26 11:59:27 -0700

    Added alpha-04 account

--- terraform/accounts-alpha.tf
+++ terraform/accounts-alpha.tf
@@ -90,3 +90,34 @@ module "charlie-aft-test-alpha-03" {
 
   account_customizations_name = "sandbox-customizations"
 }
+
+module "charlie-aft-test-alpha-04" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "charlie-aft-test-alpha-04@willardstanley.com"
+    AccountName  = "charlie-aft-test-alpha-04"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Alpha (ou-8p8k-5szl6fnl)"
+    # Syntax for nested OU
+    SSOUserEmail     = "charlie-aft-test-alpha-04@willardstanley.com"
+    SSOUserFirstName = "Alpha"
+    SSOUserLastName  = "Four"
+  }
+
+  account_tags = {
+    "AFT:test"        = "demo"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Charlie"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "sandbox-customizations"
+}
